142c365b0a847faa81b52ccc9ae7a53f5ecd1008
muhammadakbaar <mohammadakbaar11@gmail.com>
2020-05-07 15:48:18 +0700

    push pertama

---
+++ src/com/Belajar/Main.java
@@ -0,0 +1,21 @@
+package com.Belajar;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        byte a = 6;
+        int b = 5;
+        long c = 7;
+
+        float d = 3;
+        double e = 4;
+
+        boolean f = true;
+        boolean g = false;
+
+        System.out.println(a+ " " +b);
+
+
+    }
+}
